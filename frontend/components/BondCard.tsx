b0200d7e1707550a512d9a8ffe46e4398bfc7fed
Jakub Przybytek <jakub.przybytek@wundermanthompson.com>
2022-09-26 18:54:07 +0200

    Bond Card introduced

---
+++ frontend/components/BondCard.tsx
@@ -0,0 +1,80 @@
+import Stack from "@mui/material/Stack";
+import Divider from '@mui/material/Divider';
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import { BondReport } from "../sdk/GetBonds";
+
+type BondCardEntryParam = {
+  caption: string;
+  textAlign?: 'left' | 'center' | 'end';
+  children: React.ReactNode;
+}
+
+function BondCardEntry({ caption, textAlign = 'left', children }: BondCardEntryParam): JSX.Element {
+  return (
+    <Stack sx={{
+      '& > span': { textAlign: textAlign }
+    }}>
+      <Typography component='span' variant='caption'>{caption}</Typography>
+      <Typography component='span'>{children}</Typography>
+    </Stack>
+  );
+}
+
+type BondCardSectionParam = {
+  children: React.ReactNode;
+}
+
+function BondCardSection({ children }: BondCardSectionParam): JSX.Element {
+  return (
+    <Stack direction='row' sx={{
+      p: 1,
+      pb: 0,
+      justifyContent: 'space-between'
+    }}>
+      {children}
+    </Stack>
+  );
+}
+
+type BondCardParam = {
+  bond: BondReport;
+}
+
+export default function BondCard({ bond }: BondCardParam): JSX.Element {
+  return (
+    <>
+      <Paper sx={{
+        pb: 1,
+        '& .MuiTypography-caption': {
+          color: 'gray',
+          lineHeight: 1.3
+        }
+      }}>
+        <BondCardSection>
+          <Typography variant='h4'>{bond.details.name}</Typography>
+        </BondCardSection>
+        <BondCardSection>
+          <BondCardEntry caption='Market'>{bond.details.market}</BondCardEntry>
+          <BondCardEntry caption='Issuer' textAlign='center'>{bond.details.issuer}</BondCardEntry>
+          <BondCardEntry caption='Type' textAlign='end'>{bond.details.type}</BondCardEntry>
+        </BondCardSection>
+        <BondCardSection>
+          <BondCardEntry caption='Maturity day'>{bond.details.maturityDay.toString().substring(0, 10)}</BondCardEntry>
+          <BondCardEntry caption='Nominal value' textAlign='center'>{bond.details.nominalValue.toFixed(2)}</BondCardEntry>
+          <BondCardEntry caption='Interest Type' textAlign='end'>{bond.details.interestType}</BondCardEntry>
+        </BondCardSection>
+        <Divider />
+        <BondCardSection>
+          <BondCardEntry caption='Current interest'>{bond.details.currentInterestRate.toFixed(2)}%</BondCardEntry>
+          <Divider orientation='vertical' variant='middle' flexItem />
+          <BondCardEntry caption='Accured interest' textAlign='center'>{bond.details.accuredInterest.toFixed(2)}</BondCardEntry>
+          <Divider orientation='vertical' variant='middle' flexItem />
+          <BondCardEntry caption='Closing price' textAlign='center'>{bond.closingPrice.toFixed(2)}</BondCardEntry>
+          <Divider orientation='vertical' variant='middle' flexItem />
+          <BondCardEntry caption='Closing price YTM' textAlign='end'>{(bond.closingPriceYtm.ytm * 100).toFixed(2)}%</BondCardEntry>
+        </BondCardSection>
+      </Paper>
+    </>
+  );
+}
--- frontend/components/BondsList.tsx
+++ frontend/components/BondsList.tsx
@@ -1,10 +1,6 @@
-import Box from "@mui/material/Box";
-import Stack from "@mui/material/Stack";
-import Grid from "@mui/material/Grid";
-import Divider from '@mui/material/Divider';
-import Paper from "@mui/material/Paper";
-import Typography from "@mui/material/Typography";
+import BondCard from "./BondCard";
 import { BondReport } from "../sdk/GetBonds";
+import { Grid } from "@mui/material";
 
 type BondsListParam = {
   bondReports: BondReport[];
@@ -12,114 +8,16 @@ type BondsListParam = {
 
 export default function BondsList({ bondReports }: BondsListParam): JSX.Element {
   return (
-    <Box sx={{
-      '& > div': {
-        m: 1,
-        p: 1
-      }
-    }}>
+    <Grid container sx={{ pr: 1 }}>
       {bondReports.map((bond) => (
-        <Paper key={`${bond.details.name}#${bond.details.market}`}>
-          <Grid container sx={{
-            '& > .MuiGrid-container': {
-              justifyContent: 'space-between',
-              pl: 1,
-              '& > *': {
-                display: 'flex',
-                pr: 1
-              },
-              '& span': {
-                textAlign: 'end'
-              },
-              '& .MuiTypography-caption': {
-                color: 'gray',
-                lineHeight: 1.3
-              },
-              '& .MuiTypography-body1': {
-                pb: 0.4
-              }
-            }
+        <Grid item xs={12} md={6}
+          sx={{
+            pb: 1,
+            pl: 1
           }}>
-            <Grid item container direction='row' xs={12} sm={4} sx={{
-              '& > div': {
-                width: '100%',
-                justifyContent: 'space-between'
-              }
-            }}>
-              <Stack direction='row'>
-                <Typography variant='h4'>{bond.details.name}</Typography>
-                <Stack>
-                  <Typography component='span' variant='caption'>Makret</Typography>
-                  <Typography component='span'>{bond.details.market}</Typography>
-                </Stack>
-              </Stack>
-              <Stack direction='row'>
-                <Stack>
-                  <Typography component='span' variant='caption'>Issuer</Typography>
-                  <Typography component='span'>{bond.details.issuer}</Typography>
-                </Stack>
-                <Stack>
-                  <Typography component='span' variant='caption'>Type</Typography>
-                  <Typography component='span'>{bond.details.type}</Typography>
-                </Stack>
-              </Stack>
-            </Grid>
-            <Grid item container direction='row' xs={12} sm={4} sx={{
-              '& > div': {
-                width: '100%',
-                justifyContent: 'space-between'
-              }
-            }}>
-              <Stack direction='row'>
-                <Stack>
-                  <Typography component='span' variant='caption'>Maturity day</Typography>
-                  <Typography component='span'><>{bond.details.maturityDay.toString().substring(0, 10)}</></Typography>
-                </Stack>
-                <Stack>
-                  <Typography component='span' variant='caption'>Nominal value</Typography>
-                  <Typography component='span'>{bond.details.nominalValue.toFixed(2)}</Typography>
-                </Stack>
-              </Stack>
-              <Stack direction='row'>
-                <Stack>
-                  <Typography component='span' variant='caption'>Interest Type</Typography>
-                  <Typography component='span'>{bond.details.interestType}</Typography>
-                </Stack>
-              </Stack>
-            </Grid>
-            <Divider style={{ width: '100%' }} /> 
-            <Grid item container xs={12} sm={4} sx={{
-              '& > div': {
-                width: '100%',
-                justifyContent: 'space-between'
-              }
-            }}>
-              <Stack direction='row'>
-                <Stack>
-                  <Typography component='span' variant='caption'>Current interest</Typography>
-                  <Typography component='span'>{bond.details.currentInterestRate.toFixed(2)}%</Typography>
-                </Stack>
-                <Divider orientation='vertical' flexItem />
-                <Stack>
-                  <Typography component='span' variant='caption'>Accured interest</Typography>
-                  <Typography component='span'>{bond.details.accuredInterest.toFixed(2)}</Typography>
-                </Stack>
-              </Stack>
-              <Stack direction='row'>
-                <Stack>
-                  <Typography component='span' variant='caption'>Closing price</Typography>
-                  <Typography component='span'>{bond.closingPrice.toFixed(2)}</Typography>
-                </Stack>
-                <Stack>
-                  <Typography component='span' variant='caption'>Closing price YTM</Typography>
-                  <Typography component='span'>{(bond.closingPriceYtm.ytm * 100).toFixed(2)}%</Typography>
-                </Stack>
-              </Stack>
-            </Grid>
-          </Grid>
-        </Paper>
-      ))
-      }
-    </Box >
+          <BondCard key={`${bond.details.name}#${bond.details.market}`} bond={bond} />
+        </Grid>
+      ))}
+    </Grid>
   );
 }
