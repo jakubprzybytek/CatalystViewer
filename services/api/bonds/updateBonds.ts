14fc2ccacfead7fead523b99f521bb1ea877117d
jakub.przybytek <jakub.przybytek@wundermanthompson.com>
2022-10-16 10:37:24 +0200

    New bonds update procedure

--- services/api/bonds/updateBonds.ts
+++ services/api/bonds/updateBonds.ts
@@ -1,13 +1,16 @@
+import * as R from 'ramda';
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { parseUTCDate } from 'bonds';
 import { getTime } from 'date-fns';
-import { CatalystDailyStatisticsBondDetails, getLatestCatalystDailyStatistics } from '../../bonds/catalyst';
+import { CatalystBondQuery as CatalystBondQuote, CatalystDailyStatisticsBondDetails, getCurrentCatalystBondsQuotes, getLatestCatalystDailyStatistics } from '../../bonds/catalyst';
 import { getBondInformation } from '../../bonds/obligacjepl';
 import { BondDetailsTable, DbBondDetails } from '../../storage';
 
 const dynamoDbClient = new DynamoDBClient({});
 
-const bondId = (bond: DbBondDetails) => `${bond.name}#${bond.market}`;
+const bondDbId = (bond: DbBondDetails): string => `${bond.name}#${bond.market}`;
+const bondStatId = (bond: CatalystDailyStatisticsBondDetails): string => `${bond.name}#${bond.market}`;
+const mapByBondId = R.reduce((map: Record<string, DbBondDetails>, curr: DbBondDetails) => R.assoc(bondDbId(curr), curr, map), {});
 
 export async function handler(event: any) {
     if (process.env.BOND_DETAILS_TABLE_NAME === undefined) {
@@ -15,71 +18,85 @@ export async function handler(event: any) {
     }
 
     const currentTime = new Date().toISOString();
+    const bondsFailed: string[] = [];
+
     const bondDetailsTable = new BondDetailsTable(dynamoDbClient, process.env.BOND_DETAILS_TABLE_NAME);
 
     const bondsStats: CatalystDailyStatisticsBondDetails[] = await getLatestCatalystDailyStatistics();
+    //const bondsQuotes: CatalystBondQuote[] = await getCurrentCatalystBondsQuotes();
 
-    const bondsToStore: DbBondDetails[] = [];
-    const bondsFailed: string[] = [];
+    const storedBondsList: DbBondDetails[] = await bondDetailsTable.getAll();
+    const storedBonds: Record<string, DbBondDetails> = mapByBondId(storedBondsList);
+
+    const newBondsToStore: DbBondDetails[] = [];
+    const updatedBondsToStore: DbBondDetails[] = [];
+
     for (const bondStats of bondsStats) {
         try {
-            const bondInformation = await getBondInformation(bondStats.name);
+            const storedBond = storedBonds[bondStatId(bondStats)];
 
-            bondsToStore.push({
-                status: 'active',
-                updated: currentTime,
-                name: bondStats.name,
-                isin: bondStats.isin,
-                market: bondStats.market,
-                issuer: bondInformation.issuer,
-                type: bondStats.type,
-                nominalValue: bondStats.nominalValue,
-                currency: bondStats.tradingCurrency,
-                maturityDay: bondStats.maturityDay,
-                maturityDayTs: bondStats.maturityDay.getTime(),
-                interestType: bondInformation.interestType,
-                interestVariable: bondInformation.interestVariable,
-                interestConst: bondInformation.interestConst,
-                currentInterestRate: bondStats.currentInterestRate,
-                accuredInterest: bondStats.accuredInterest,
-                closingPrice: bondStats.closingPrice,
-                interestFirstDays: bondInformation.interestFirstDays,
-                interestFirstDayTss: bondInformation.interestFirstDays.map(parseUTCDate).map(getTime),
-                interestRightsDays: bondInformation.interestRightsDays,
-                interestRightsDayTss: bondInformation.interestRightsDays.map(parseUTCDate).map(getTime),
-                interestPayoffDays: bondInformation.interestPayoffDays,
-                interestPayoffDayTss: bondInformation.interestPayoffDays.map(parseUTCDate).map(getTime)
-            });
+            if (storedBond !== undefined) {
+                // update existing bond information
+                updatedBondsToStore.push({
+                    ...storedBond,
+                    updated: currentTime,
+                    currentInterestRate: bondStats.currentInterestRate,
+                    accuredInterest: bondStats.accuredInterest,
+                    closingPrice: bondStats.closingPrice,
+                });
+            } else {
+                // create new bond information record
+                const bondInformation = await getBondInformation(bondStats.name);
+
+                newBondsToStore.push({
+                    status: 'active',
+                    updated: currentTime,
+                    name: bondStats.name,
+                    isin: bondStats.isin,
+                    market: bondStats.market,
+                    issuer: bondInformation.issuer,
+                    type: bondStats.type,
+                    nominalValue: bondStats.nominalValue,
+                    currency: bondStats.tradingCurrency,
+                    maturityDay: bondStats.maturityDay,
+                    maturityDayTs: bondStats.maturityDay.getTime(),
+                    interestType: bondInformation.interestType,
+                    interestVariable: bondInformation.interestVariable,
+                    interestConst: bondInformation.interestConst,
+                    currentInterestRate: bondStats.currentInterestRate,
+                    accuredInterest: bondStats.accuredInterest,
+                    closingPrice: bondStats.closingPrice,
+                    interestFirstDays: bondInformation.interestFirstDays,
+                    interestFirstDayTss: bondInformation.interestFirstDays.map(parseUTCDate).map(getTime),
+                    interestRightsDays: bondInformation.interestRightsDays,
+                    interestRightsDayTss: bondInformation.interestRightsDays.map(parseUTCDate).map(getTime),
+                    interestPayoffDays: bondInformation.interestPayoffDays,
+                    interestPayoffDayTss: bondInformation.interestPayoffDays.map(parseUTCDate).map(getTime)
+                });
+            }
         } catch (error: any) {
             bondsFailed.push(bondStats.name);
             console.error(error);
         }
     }
 
-    const newAndUpdatedBondsIdies = bondsToStore.map(bondId);
-
-    const storedBonds = await bondDetailsTable.getAll();
-    const storedBondsIdies = storedBonds.map(bondId);
-
-    const newBonds = bondsToStore
-        .filter((bondToStore) => !storedBondsIdies.includes(bondId(bondToStore)));
-
-    const bondsToDeactivate = storedBonds
-        .filter((storedBond) => !newAndUpdatedBondsIdies.includes(bondId(storedBond)));
-
-    bondsToDeactivate.forEach((bondToDeactivate) => {
-        bondsToStore.push({
-            ...bondToDeactivate,
+    // look for bonds to deactivate
+    const newAndUpdatedBondIdies = bondsStats.map(bondStatId);
+    const deactivatedBondsToStore = storedBondsList
+        .filter(bond => !newAndUpdatedBondIdies.includes(bondDbId(bond)))
+        .map(bond => ({
+            ...bond,
             status: 'inactive'
-        })
-    });
+        }));
 
-    await bondDetailsTable.storeAll(bondsToStore);
+    bondDetailsTable.storeAll(updatedBondsToStore
+        .concat(newBondsToStore)
+        .concat(deactivatedBondsToStore));
 
     return {
-        bondsUpdated: bondsToStore.length,
-        newBonds: newBonds.map((bond) => bond.name),
-        bondsDeactivated: bondsToDeactivate.map((bond) => bond.name),
+        bondsUpdated: updatedBondsToStore.length,
+        newBonds: newBondsToStore.map(bond => bond.name),
+        bondsDeactivated: deactivatedBondsToStore.map(bond => bond.name),
         bondsFailed
     }
 }
