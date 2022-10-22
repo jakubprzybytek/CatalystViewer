d2155109dab780a1d41cd23682759fd7e22839b8
jakub.przybytek <jakub.przybytek@wundermanthompson.com>
2022-10-22 19:24:08 +0200

    More headers

--- services/bonds/catalyst/fetch/CatalystFetch.ts
+++ services/bonds/catalyst/fetch/CatalystFetch.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 
 export async function fetchBondsQuotes(path: string): Promise<string> {
     const url = `https://gpwcatalyst.pl${path}`;
@@ -12,9 +12,18 @@ export async function fetchBondsQuotes(path: string): Promise<string> {
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
             'Accept-Encoding': 'gzip, deflate, br',
             'Accept-Language': 'en-GB,en;q=0.9,en-US;q=0.8,pl;q=0.7',
+            'Cache-Control': 'max-age=0',
+            'Connection': 'keep-alive',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
         }
-    }).then((response) => response.data);
+    })
+        .then(response => response.data)
+        .catch(error => {
+            if (axios.isAxiosError(error))
+                throw Error(`Cannot fetch '${url}': ${(error as AxiosError).code}`);
+            else
+                throw Error(`Cannot fetch '${url}': ${error}`);
+        });
 }
 
 export async function fetchCorporateBondsQuotes(): Promise<string> {
