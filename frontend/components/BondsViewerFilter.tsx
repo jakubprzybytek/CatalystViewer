6366d865c5447aa1679599ab42e05769a38d0cff
jakub.przybytek <jakub.przybytek@wundermanthompson.com>
2022-10-25 00:32:17 +0200

    Better issuer selector

--- frontend/components/BondsViewerFilter.tsx
+++ frontend/components/BondsViewerFilter.tsx
@@ -9,12 +9,17 @@ import FormGroup from '@mui/material/FormGroup';
 import FormLabel from '@mui/material/FormLabel';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
-import Select, { SelectChangeEvent } from '@mui/material/Select';
 import TextField from '@mui/material/TextField';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
 import MenuItem from '@mui/material/MenuItem';
 import Chip from '@mui/material/Chip';
 import Link from '@mui/material/Link';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
+import Check from '@mui/icons-material/Check';
+import AddCircle from '@mui/icons-material/AddCircle';
 import { BondReport, BondDetails } from '../sdk/GetBonds';
 import { useArrayLocalStorage, useLocalStorage } from '../common/UseStorage';
 
@@ -45,6 +50,8 @@ const defaultMarkets = ['GPW RR', 'GPW ASO'];
 
 export default function BondsViewerFilter({ allBondReports, setBondTypeFilter: setBondTypeFilter2, setFilteredBondReports: setFilteredBonds }: BondsViewerFilterParams): JSX.Element {
   const [moreFiltersExpanded, setMoreFiltersExpanded] = useState(false);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
 
   const [bondTypeFilter, setBondTypeFilter] = useLocalStorage<string>('filter.bondType', 'Corporate bonds');
   const [issuersFilter, addIssuerFilter, removeIssuerFilter] = useArrayLocalStorage('filter.issuer', defaultIssuers);
@@ -83,19 +90,23 @@ export default function BondsViewerFilter({ allBondReports, setBondTypeFilter: s
         </FormControl>
       </Grid>
       <Grid container spacing={1}>
-        <Grid item xs={12} sm={6} md={4}>
-          <FormControl fullWidth>
-            <FormLabel>Issuers</FormLabel>
-            <Select label="New" size="small" fullWidth
-              value=''
-              onChange={(event: SelectChangeEvent) => addIssuerFilter(event.target.value)}>
-              {availableIssuers.map((issuer) => (
-                <MenuItem key={issuer} value={issuer}>{issuer}</MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-        </Grid>
         <Grid item xs={12}>
+          <Button startIcon={(<AddCircle />)}
+            onClick={(event: React.MouseEvent<HTMLButtonElement>) => setAnchorEl(event.currentTarget)}>
+            Select Issuers
+          </Button>
+          <Menu anchorEl={anchorEl} open={open} onClose={() => setAnchorEl(null)}>
+            {availableIssuers.map(issuer =>
+              issuersFilter.includes(issuer) ?
+                (<MenuItem key={issuer} onClick={() => removeIssuerFilter(issuer)}>
+                  <ListItemIcon><Check /></ListItemIcon>
+                  {issuer}
+                </MenuItem>)
+                : (<MenuItem key={issuer} onClick={() => addIssuerFilter(issuer)}>
+                  <ListItemText inset>{issuer}</ListItemText>
+                </MenuItem>)
+            )}
+          </Menu>
           <Stack direction='row' flexWrap='wrap' sx={{
             '& > div': {
               mr: 1,
@@ -146,7 +157,7 @@ export default function BondsViewerFilter({ allBondReports, setBondTypeFilter: s
           </Grid>
         </Grid>
       </Collapse>
-      <Typography sx={{ ml: 2, mt: 2 }}>Listing {filteredBonds.length} bonds</Typography>
+      <Typography sx={{ mt: 2 }}>Listing {filteredBonds.length} bonds</Typography>
     </Paper >
   );
 }
