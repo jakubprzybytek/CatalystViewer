5f10dd964d0873e1f390f8fec0b5ad654c895d3d
jakub.przybytek <jakub.przybytek@wundermanthompson.com>
2022-10-27 22:50:24 +0200

    Load more

--- frontend/components/BondsList.tsx
+++ frontend/components/BondsList.tsx
@@ -1,8 +1,12 @@
-import BondCard from "./BondCard";
-import { BondReport } from "../sdk/GetBonds";
+import { useEffect, useState } from "react";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import BondCard from "./BondCard";
+import { BondReport } from "../sdk/GetBonds";
 import { BondsStatistics } from '../common/BondsStatistics';
+
+const BATCH_SIZE = 20;
 
 type BondsListParam = {
   bondReports: BondReport[];
@@ -10,14 +14,25 @@ type BondsListParam = {
 }
 
 export default function BondsList({ bondReports, bondsStatistics }: BondsListParam): JSX.Element {
+  const [displayedItemsCount, setDisplayedItemsCound] = useState(BATCH_SIZE);
+
+  useEffect(() => setDisplayedItemsCound(BATCH_SIZE), [bondReports]);
+
   return (
     <Box>
       <Grid container spacing={1}>
-        {bondReports.slice(0, 20).map((bondReportr) => (
+        {bondReports.slice(0, displayedItemsCount).map((bondReportr) => (
           <Grid key={`${bondReportr.details.name}#${bondReportr.details.market}`} item xs={12} sm={6} lg={4} xl={3}>
             <BondCard bondReport={bondReportr} bondsStatistics={bondsStatistics} />
           </Grid>
         ))}
+        {displayedItemsCount < bondReports.length &&
+          <Grid item container xs={12} justifyContent='center'>
+            <Button variant='outlined'
+              onClick={() => setDisplayedItemsCound(displayedItemsCount + BATCH_SIZE)}>
+              Load more
+            </Button>
+          </Grid>}
       </Grid>
     </Box>
   );
