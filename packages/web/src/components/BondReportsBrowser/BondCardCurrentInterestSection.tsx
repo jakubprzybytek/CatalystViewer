950f4f2bb1e9291e372edbc3d355ddaf47c2580a
Jakub Przybytek <jakub.przybytek@gmail.com>
2023-10-25 00:00:16 +0200

    Starting refctor of Bond Reports Browser

---
+++ packages/web/src/components/BondReportsBrowser/BondCardCurrentInterestSection.tsx
@@ -0,0 +1,63 @@
+import { styled } from '@mui/material/styles';
+import LinearProgress, { linearProgressClasses } from '@mui/material/LinearProgress';
+import { BondReport } from '../../sdk/GetBonds';
+import { formatCurrency, formatDate } from '../../common/Formats';
+import { CardSection, CardEntry, CardValue } from '../../common/Cards';
+import { ColorCode } from '../../common/ColorCodes';
+import { Box } from '@mui/material';
+import { differenceInDays } from 'date-fns';
+
+const BorderLinearProgress = styled(LinearProgress)(({ theme }) => ({
+  height: 10,
+  borderRadius: 5,
+  [`&.${linearProgressClasses.colorPrimary}`]: {
+    backgroundColor: theme.palette.grey[theme.palette.mode === 'light' ? 200 : 800],
+  },
+  // [`& .${linearProgressClasses.bar}`]: {
+  //   borderRadius: 5,
+  //   backgroundColor: theme.palette.mode === 'light' ? '#1a90ff' : '#308fe8',
+  // },
+}));
+
+type BondCardCurrentInterestSectionParams = {
+  bondReport: BondReport;
+}
+
+export default function BondCardCurrentInterestSection({ bondReport: { currentValues, details: { currency } } }: BondCardCurrentInterestSectionParams): JSX.Element {
+  const today = new Date();
+  const interestBarColor = today.getTime() >= currentValues.interestRecordDay ? 'success' : 'error';
+
+  const accuredInterestColorCode: ColorCode = currentValues.accuredInterest == 0 ? 'green' : 'none';
+
+  return (
+    <>
+      <CardSection>
+        <Box flexGrow={1}>
+          <BorderLinearProgress variant='determinate' color={interestBarColor} value={currentValues.interestProgress} />
+        </Box>
+      </CardSection>
+      <CardSection>
+        <CardEntry caption='First day' width='33%'>
+          <CardValue>{formatDate(currentValues.interestFirstDay)}</CardValue>
+        </CardEntry>
+        <CardEntry caption='Record day' textAlign='center' width='33%'>
+          <CardValue>{formatDate(currentValues.interestRecordDay)}</CardValue>
+        </CardEntry>
+        <CardEntry caption='Payable' textAlign='end' width='33%'>
+          <CardValue>{formatDate(currentValues.interestPayableDay)}</CardValue>
+        </CardEntry>
+      </CardSection>
+      <CardSection>
+        <CardEntry caption='Current interest' width='33%'>
+          <CardValue variant='h6'>{currentValues.interestRate.toFixed(2)}%</CardValue>
+        </CardEntry>
+        <CardEntry caption='Accured interest' textAlign='center' width='33%'>
+          <CardValue colorCode={accuredInterestColorCode}>{formatCurrency(currentValues.accuredInterest, currency)}</CardValue>
+        </CardEntry>
+        <CardEntry caption='Full interest' textAlign='end' width='33%'>
+          <CardValue>{formatCurrency(currentValues.periodInterest, currency)}</CardValue>
+        </CardEntry>
+      </CardSection>
+    </>
+  );
+}
--- packages/web/src/components/BondReportsBrowser/BondReportsBrowser.tsx
+++ packages/web/src/components/BondReportsBrowser/BondReportsBrowser.tsx
@@ -1,14 +1,58 @@
+import { useEffect, useMemo, useState } from "react";
 import Box from "@mui/material/Box";
-import { BondReport } from "@/sdk/GetBonds";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
+import { BondReport, getBonds } from "@/sdk/GetBonds";
+import BondsList from "./BondsList";
+import { computeStatisticsForInterestBaseTypes } from "@/bonds/statistics";
 
 type BondReportsBrowserParam = {
   bondReports: BondReport[];
 }
 
 export default function BondReportsBrowser(/*{ bondReports }: BondReportsBrowserParam*/): JSX.Element {
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState<string | undefined>();
+
+  const [allBondReports, setAllBondReports] = useState<BondReport[]>([]);
+  const [allBondTypes, setAllBondTypes] = useState<string[]>([]);
+
+  const filteredBondReports = allBondReports;
+
+  const filteredBondsStatistics = useMemo(() => computeStatisticsForInterestBaseTypes(filteredBondReports), [filteredBondReports]);
+
+  async function fetchData(bondType: string) {
+    console.log(`Fetching reports for bond type: ${bondType}`);
+    try {
+      const bondsResponse = await getBonds(bondType);
+      setErrorMessage(undefined);
+      setAllBondReports(bondsResponse.bondReports);
+      setAllBondTypes(bondsResponse.facets.type);
+      console.log(`Fetched '${bondsResponse.bondReports.length}' bond reports`);
+    } catch (error) {
+      if (error instanceof Error) {
+        setErrorMessage(error.message);
+        setAllBondReports([]);
+      } else {
+        setErrorMessage(Object(error));
+        setAllBondReports([]);
+      }
+    }
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetchData('Corporate bonds');
+  }, ['Corporate bonds']);
+  
   return (
     <Box>
-      Bond Reports Browser
+      <BondsList bondReports={filteredBondReports} statistics={filteredBondsStatistics} />
+      {errorMessage && <Alert severity="error">
+        <AlertTitle>Cannot fetch data!</AlertTitle>
+        <pre>{errorMessage}</pre>
+      </Alert>}
     </Box>
   );
 }
