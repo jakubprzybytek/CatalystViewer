75a5153527da6e09f1650be7ea562bb7a900620a
jakub.przybytek <jakub.przybytek@wundermanthompson.com>
2023-11-02 15:55:45 +0100

    Filter options modifiers refactor

--- packages/web/src/components/BondReportsBrowser/filter/BondReportsFilterPanel.tsx
+++ packages/web/src/components/BondReportsBrowser/filter/BondReportsFilterPanel.tsx
@@ -3,7 +3,7 @@ import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import { getUniqueInterestBaseTypes, getUniqueIssuers, getUniqueMarkets, sortStrings } from "@/bonds/statistics";
 import { BondReport } from "@/sdk/GetBonds";
-import { BondReportsFilteringOptions } from ".";
+import { BondReportsFilteringOptions, marketsModifiers, interestBaseTypesModifiers, issuersModifiers, bondTypeModifier, maxNominalValueModifier } from ".";
 import { NominalValueFilter, StringFilter, MultiStringFilter } from "./fields";
 import IssuersSelector from "./IssuersSelector";
 
@@ -17,40 +17,26 @@ type BondReportsFilterPanelParams = {
 export function BondReportsFilterPanel({ allBondReports, allBondTypes, filteringOptions, setFilteringOptions }: BondReportsFilterPanelParams): JSX.Element {
   const allInterestBaseTypes = useMemo(() => sortStrings(getUniqueInterestBaseTypes(allBondReports)), [allBondReports]);
   const allMarkets = useMemo(() => sortStrings(getUniqueMarkets(allBondReports)), [allBondReports]);
+  const allIssuers = useMemo(() => getUniqueIssuers(allBondReports), [allBondReports]);
 
-  const removeElement = (array: string[], elementToRemove: string) => {
-    const index = array.indexOf(elementToRemove, 0);
-    const newArray = [...array];
-    if (index > -1) {
-      newArray.splice(index, 1);
-    }
-    return newArray;
-  }
-
-  const setFilteringOption = (newValue: Partial<BondReportsFilteringOptions>) => setFilteringOptions({ ...filteringOptions, ...newValue });
-
-  const addMarket = (newMarket: string) => setFilteringOption({ markets: [...filteringOptions.markets, newMarket] });
-  const removeMarket = (marketToRemove: string) => setFilteringOption({ markets: removeElement(filteringOptions.markets, marketToRemove) });
-  const addInterestBaseTyp = (newInterestBaseType: string) => setFilteringOption({ interestBaseTypes: [...filteringOptions.interestBaseTypes, newInterestBaseType] });
-  const removeInterestBasetType = (interestBaseTypeToRemove: string) => setFilteringOption({ interestBaseTypes: removeElement(filteringOptions.interestBaseTypes, interestBaseTypeToRemove) });
-
-  const allIssuers = getUniqueIssuers(allBondReports);
-  const addIssuer = (newIssuer: string) => setFilteringOption({ issuers: [...filteringOptions.issuers, newIssuer] });
-  const removeIssuer = (issuerToRemove: string) => setFilteringOption({ issuers: removeElement(filteringOptions.issuers, issuerToRemove) });
-  const removeAllIssuers = () => setFilteringOption({ issuers: [] });
+  const setBondType = bondTypeModifier(filteringOptions, setFilteringOptions);
+  const setMaxNominal = maxNominalValueModifier(filteringOptions, setFilteringOptions);
+  const { addMarket, removeMarket } = marketsModifiers(filteringOptions, setFilteringOptions);
+  const { addInterestBaseTyp, removeInterestBasetType } = interestBaseTypesModifiers(filteringOptions, setFilteringOptions);
+  const { addIssuer, removeIssuer, removeAllIssuers } = issuersModifiers(filteringOptions, setFilteringOptions);
 
   return (
     <>
       <Grid container item xs={12} sm={6} md={4} direction='column'>
         <Typography paddingBottom={1}>Select bond type:</Typography>
         <StringFilter label='Bond Type'
-          all={allBondTypes} selected={filteringOptions.bondType} setSelected={(newBondType: string) => setFilteringOption({ bondType: newBondType })} />
+          all={allBondTypes} selected={filteringOptions.bondType} setSelected={setBondType} />
       </Grid>
       <Grid container item marginTop={1}>
         <Typography paddingBottom={1}>Apply more filters:</Typography>
         <Grid container item spacing={1}>
           <Grid item xs={12} sm={6} md={4}>
-            <NominalValueFilter selectedNominalValue={filteringOptions.maxNominal} setSelectedNominalValue={(newMaxNominal: number) => setFilteringOption({ maxNominal: newMaxNominal })} />
+            <NominalValueFilter selectedNominalValue={filteringOptions.maxNominal} setSelectedNominalValue={setMaxNominal} />
           </Grid>
           <Grid item xs={12} sm={6} md={4}>
             <MultiStringFilter label='Market'
--- packages/web/src/components/BondReportsBrowser/filter/index.ts
+++ packages/web/src/components/BondReportsBrowser/filter/index.ts
@@ -22,3 +22,42 @@ export function filterUsing(filteringOptions: BondReportsFilteringOptions) {
     isIssuedBy(filteringOptions.issuers)
   ])
 };
+
+const removeElement = (array: string[], elementToRemove: string) => {
+  const index = array.indexOf(elementToRemove, 0);
+  const newArray = [...array];
+  if (index > -1) {
+    newArray.splice(index, 1);
+  }
+  return newArray;
+}
+
+export function bondTypeModifier(filteringOptions: BondReportsFilteringOptions, setFilteringOptions: (newFilteringOptions: BondReportsFilteringOptions) => void) {
+  return (newBondType: string) => setFilteringOptions({ ...filteringOptions, bondType: newBondType });
+}
+
+export function maxNominalValueModifier(filteringOptions: BondReportsFilteringOptions, setFilteringOptions: (newFilteringOptions: BondReportsFilteringOptions) => void) {
+  return (newMaxNominalValue: number) => setFilteringOptions({ ...filteringOptions, maxNominal: newMaxNominalValue });
+}
+
+export function marketsModifiers(filteringOptions: BondReportsFilteringOptions, setFilteringOptions: (newFilteringOptions: BondReportsFilteringOptions) => void) {
+  return {
+    addMarket: (newMarket: string) => setFilteringOptions({ ...filteringOptions, markets: [...filteringOptions.markets, newMarket] }),
+    removeMarket: (marketToRemove: string) => setFilteringOptions({ ...filteringOptions, markets: removeElement(filteringOptions.markets, marketToRemove) })
+  }
+}
+
+export function interestBaseTypesModifiers(filteringOptions: BondReportsFilteringOptions, setFilteringOptions: (newFilteringOptions: BondReportsFilteringOptions) => void) {
+  return {
+    addInterestBaseTyp: (newInterestBaseType: string) => setFilteringOptions({ ...filteringOptions, interestBaseTypes: [...filteringOptions.interestBaseTypes, newInterestBaseType] }),
+    removeInterestBasetType: (interestBaseTypeToRemove: string) => setFilteringOptions({ ...filteringOptions, interestBaseTypes: removeElement(filteringOptions.interestBaseTypes, interestBaseTypeToRemove) })
+  }
+}
+
+export function issuersModifiers(filteringOptions: BondReportsFilteringOptions, setFilteringOptions: (newFilteringOptions: BondReportsFilteringOptions) => void) {
+  return {
+    addIssuer: (newIssuer: string) => setFilteringOptions({ ...filteringOptions, issuers: [...filteringOptions.issuers, newIssuer] }),
+    removeIssuer: (issuerToRemove: string) => setFilteringOptions({ ...filteringOptions, issuers: removeElement(filteringOptions.issuers, issuerToRemove) }),
+    removeAllIssuers: () => setFilteringOptions({ ...filteringOptions, issuers: [] })
+  }
+}
