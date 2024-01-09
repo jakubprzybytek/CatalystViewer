d72dcc3cd8ce5c47ca5b18a0cc091a99f416d5ef
Jakub Przybytek <jakub.przybytek@gmail.com>
2024-01-09 19:31:30 +0100

    Trading days ratio color coding

--- packages/web/src/components/BondReportsBrowser/view/BondCardLiquiditySection.tsx
+++ packages/web/src/components/BondReportsBrowser/view/BondCardLiquiditySection.tsx
@@ -4,6 +4,7 @@ import { BondReport } from '@/sdk/Bonds';
 import { CardSection, CardEntry, CardValue } from '@/common/Cards';
 import { formatCompactCurrency, formatCurrency } from '@/common/Formats';
 import BondLiquidityDialog from './BondLiquidityDialog';
+import { ColorCode } from '@/common/ColorCodes';
 
 type BondCardLiquiditySectionParams = {
   bondReport: BondReport;
@@ -13,6 +14,8 @@ export default function BondCardLiquiditySection({ bondReport }: BondCardLiquidi
   const { currentValues, details } = bondReport;
   const [liquidityReport, setLiquidityReport] = useState<BondReport | undefined>(undefined);
 
+  const tradingDaysColorCode: ColorCode = currentValues.tradingDaysRatio > 0.75 ? 'green' : currentValues.tradingDaysRatio > 0.5 ? 'yellow' : currentValues.tradingDaysRatio > 0.25 ? 'orange' : 'red';
+
   return (
     <CardSection>
       {liquidityReport && <BondLiquidityDialog bondReport={liquidityReport} onClose={() => setLiquidityReport(undefined)} />}
@@ -20,7 +23,7 @@ export default function BondCardLiquiditySection({ bondReport }: BondCardLiquidi
         <CardValue>{!!currentValues.averageTurnover ? formatCompactCurrency(currentValues.averageTurnover * 1000, details.currency) : 'N/A'}</CardValue>
       </CardEntry>
       <CardEntry caption='Trading ratio' textAlign='center' width='25%'>
-        <CardValue>{Math.round(currentValues.tradingDaysRatio * 100)}%</CardValue>
+        <CardValue colorCode={tradingDaysColorCode}>{Math.round(currentValues.tradingDaysRatio * 100)}%</CardValue>
       </CardEntry>
       <CardEntry caption='Avg spread' textAlign='end' width='25%'>
         <CardValue>{!!currentValues.averageSpread ? formatCurrency(currentValues.averageSpread, details.currency) : 'N/A'}</CardValue>
