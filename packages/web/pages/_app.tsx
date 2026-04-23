9c4f05626173b5b494ad7c36682c67e44ef13809
Jakub Przybytek <jakub.przybytek@gmail.com>
2026-04-23 10:18:01 +0000

    Local fonts

--- packages/web/pages/_app.tsx
+++ packages/web/pages/_app.tsx
@@ -1,7 +1,18 @@
 import '../styles/globals.css';
 import React from 'react';
 import type { AppProps } from 'next/app';
+import localFont from 'next/font/local';
 import { Amplify } from "aws-amplify";
+
+const roboto = localFont({
+  display: 'swap',
+  src: [
+    { path: '../public/fonts/roboto/roboto-latin-300-normal.woff2', weight: '300', style: 'normal' },
+    { path: '../public/fonts/roboto/roboto-latin-400-normal.woff2', weight: '400', style: 'normal' },
+    { path: '../public/fonts/roboto/roboto-latin-500-normal.woff2', weight: '500', style: 'normal' },
+    { path: '../public/fonts/roboto/roboto-latin-700-normal.woff2', weight: '700', style: 'normal' },
+  ],
+});
 
 const amplifyConfig = {
   Auth: {
@@ -28,5 +39,5 @@ Amplify.configure(amplifyConfig);
 
 export default function App({ Component, pageProps }: AppProps) {
   const Page = Component as React.ComponentType<typeof pageProps>;
-  return <Page {...pageProps} />
+  return <Page className={roboto.className} {...pageProps} />;
 }
